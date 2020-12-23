0c331424f31f473074fd5170a1839a787c01185d
kim <kingxt4job@gmail.com>
2020-12-23 09:42:33 +0800

    parse api

--- tools/goctl/api/parser/parser.go
+++ tools/goctl/api/parser/parser.go
@@ -1,56 +1,12 @@
 package parser
 
 import (
-	"errors"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
-
 	"github.com/tal-tech/go-zero/tools/goctl/api/parser/g4/ast"
 	"github.com/tal-tech/go-zero/tools/goctl/api/spec"
-	"github.com/tal-tech/go-zero/tools/goctl/util"
 )
 
 func Parser(filename string) (*spec.ApiSpec, error) {
-	apiAbsPath, err := filepath.Abs(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	api, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	parser := ast.NewParser(string(api))
-	visitor := ast.NewApiVisitor()
-	apiSpec := parser.Api().Accept(visitor).(spec.ApiSpec)
-
-	for _, item := range apiSpec.Import.List {
-		importFile := strings.TrimSpace(item)
-		if len(importFile) > 0 {
-			item = strings.TrimSpace(item)
-			var path = item
-			if !util.FileExists(item) {
-				path = filepath.Join(filepath.Dir(apiAbsPath), item)
-			}
-			content, err := ioutil.ReadFile(path)
-			if err != nil {
-				return nil, errors.New("import api file not exist: " + item)
-			}
-
-			parser := ast.NewParser(string(content))
-			visitor := ast.NewApiVisitor()
-			importApiSpec := parser.Api().Accept(visitor).(spec.ApiSpec)
-
-			if len(importApiSpec.Import.List) > 0 {
-				return nil, errors.New("import api should not import another api file recursive")
-			}
-
-			apiSpec.Types = append(apiSpec.Types, importApiSpec.Types...)
-			apiSpec.Service.Groups = append(apiSpec.Service.Groups, importApiSpec.Service.Groups...)
-		}
-	}
-
-	return &apiSpec, nil
+	parser := ast.NewParser()
+	apiSpec, err := parser.Parse(filename)
+	return apiSpec, err
 }
