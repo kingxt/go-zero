f6d62613f0bf60a9097a9aa1301b17f66a04efc9
anqiansong <anqiansong@xiaoheiban.cn>
2021-01-06 13:35:23 +0800

    add grammar test

---
+++ tools/goctl/api/parser/g4/test/grammar_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var files = []string{
+	"example",
+	"empty",
+	"syntax",
+	"info",
+}
+
+func TestGrammar(t *testing.T) {
+	for _, file := range files {
+		t.Run(file, func(t *testing.T) {
+			_, err := parser.Parse("./apis/" + file + ".api")
+			assert.Nil(t, err)
+		})
+	}
+}
